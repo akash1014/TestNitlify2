7066abcb2aa96528ba138441df4309858e83907c
akash1014 <48207918+akash1014@users.noreply.github.com>
2021-11-06 16:46:07 +0530

    Sample demo  changes
    Changes made for demo of the webpage to be shown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  frontend_img='../../../assets/';
   title = 'TestNitlify2';
+  
+
 }
